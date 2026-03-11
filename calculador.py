46928d96823e3a9a5402237b4f809aab57da9217
Ordep <pedro.mantovanini.silva@escola.pr.gov.br>
2026-03-11 09:52:41 -0300

    Add files via upload

---
+++ calculador.py
@@ -0,0 +1,27 @@
+#Calculadora de Aprovação Escolar
+
+nome = input("Digite o nome do estudante: ")
+
+soma_notas = 0
+
+quantidade_trimestre = 3
+
+meta_aprovacao = 180
+
+#Coleta as notas dos 3 períodos
+
+for i in range(1, quantidade_trimestre + 1): 
+ nota = float(input("Informe a nota{i} periodo:º"))
+soma_notas +=nota
+
+print("-" * 30)
+print(f"Estudante: {nome}")
+print(f"Pontuação Total: {soma_notas}")
+
+#Verifica o Status de aprovação
+if soma_notas >= meta_aprovacao:
+    print("Status: APROVADÃO! PARABÉNS!!! FINALMENTE!!!")
+else: 
+    pontos_faltantes = meta_aprovacao - soma_notas
+    print("Status: TENTE OUTRA VEZ!!")
+    print(f"Faltaram {pontos_faltantes} pontos para atingir o minimo de {meta_aprovacao}.")
